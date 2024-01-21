f0d49db4451b350a9017b116ece89a2cea10265d
Pavel Novytskyi <pavlo.novytskyi@chisw.com>
2024-01-21 22:37:33 +0200

    make better logs for connecting procces

--- parallel_computing_cw_client/parallel_computing_cw_client.cpp
+++ parallel_computing_cw_client/parallel_computing_cw_client.cpp
@@ -3,52 +3,60 @@
 
 using namespace boost::asio;
 
-using std::cout;
-using std::endl;
-
 boost::asio::io_service io;
 boost::asio::ip::tcp::socket s(io);
 
-void send() {
-    const std::string msg = "Hello from Client!";// \n";
+void send(const std::string& msg) {
+    //const std::string msg = "Hello from Client!";// \n";
     boost::system::error_code error;
     boost::asio::write(s, boost::asio::buffer(msg + "\n"), error);
     if (!error) {
-        cout << "Message to server: " << msg << endl;
+        std::cout << "Message to server: " << msg << std::endl;
     }
     else {
-        cout << "send failed: " << error.message() << endl;
+        std::cout << "send failed: " << error.message() << std::endl;
     }
 }
 
-void receive() {
+std::string receive() {
     boost::asio::streambuf receive_buffer;
     boost::system::error_code error;
     boost::asio::read(s, receive_buffer, boost::asio::transfer_all(), error);
     if (error && error != boost::asio::error::eof) {
-        cout << "receive failed: " << error.message() << endl;
+        std::cout << "receive failed: " << error.message() << std::endl;
+        return {};
     }
-    else {
-        const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
-        cout << data << endl;
-    }
+    
+    auto data = std::string(boost::asio::buffers_begin(receive_buffer.data()), boost::asio::buffers_end(receive_buffer.data()));
+    //std::cout << data << std::endl;
+    return data;
 }
 
 int main() {
     bool connected = false;
+    std::cout << "Connecting to server";
     while (!connected) {
         try {
             s.connect(ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
         }
         catch (...) {
+            std::cout << ".";
             continue;
         }
+        std::cout << "connected!\n";
         connected = true;
     }
     
     
-    while (1)
-        send();
-    receive();
+    while (1) {
+        std::string msg;
+        std::cout << "Enter command: ";
+        std::getline(std::cin, msg);
+        send(msg);
+        msg = receive();
+        std::cout << "\nGot: " << msg << std::endl;
+    }
+        
+    
     return 0;
 }
